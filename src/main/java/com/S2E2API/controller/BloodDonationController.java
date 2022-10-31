0b0f65e8059862ecfc938679424263037f21432e
yunsup <zksdbstjq@gmail.com>
2022-11-01 00:28:09 +0900

    TEST 1

--- src/main/java/com/S2E2API/controller/BloodDonationController.java
+++ src/main/java/com/S2E2API/controller/BloodDonationController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -38,12 +39,15 @@ public class BloodDonationController {
     return ResponseEntity.ok(bloodDonateService.updateDonationInfo(id));
   }
 
-  @RequestMapping(value = "/findAll")
+  @RequestMapping(value = "/findAll"  ,produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE)
   @ResponseBody
   public ResponseEntity<List<BloodDonationHouse>> getHouseInfo() {
     List<BloodDonationHouse> bloodDonationHouseList = bloodDonateService.getHouseInfo();
     return new ResponseEntity<>(bloodDonationHouseList, HttpStatus.OK);
   }
+
+  //Resolved [org.springframework.web.method.annotation.MethodArgumentTypeMismatchException: Failed to convert value of type 'java.lang.String' to required type 'java.lang.Long'; nested exception is java.lang.NumberFormatException: For input string: "findAll"]
+
 
 }
 
